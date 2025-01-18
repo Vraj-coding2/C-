b617751c57dfe2c8e6132d0190a39fb3284364cc
Vrajcoding2 <vrajgajjar659@gmail.com>
2025-01-18 15:18:48 +0530

    practical 03

---
+++ practical 03.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+int arr[] = {12,34,50,1,0};
+int length = sizeof(arr)/sizeof(arr[0]);
+int maximum = INT16_MIN,minimum = INT16_MAX;// this is a predefine value
+for(int i = 0;i < length;i++){
+    maximum = max(maximum,arr[i]);//this is a predefine function to find maximum number
+    minimum = min(minimum,arr[i]);//this is a predefine function to find minimum number
+}
+cout << "maximum number is:" << maximum << endl;
+cout << "minimum number is:" << minimum << endl;
+return 0;
+}
