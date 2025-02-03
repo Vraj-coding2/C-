81576a09c9167e52f109063381338e5c8e955baa
Vrajcoding2 <vrajgajjar659@gmail.com>
2025-02-03 18:49:26 +0530

    practical 03

--- practical 03.cpp
+++ practical 03.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 int main(){
-int arr[] = {12,34,50,1,0};
-int length = sizeof(arr)/sizeof(arr[0]);
+  int len;
+  cout << "enter the length:";
+  cin >> len;
+int arr[len];
 int maximum = INT16_MIN,minimum = INT16_MAX;// this is a predefine value
-for(int i = 0;i < length;i++){
+for(int i = 0;i < len;i++){
+   cout << "array" << '[' << i << "]:";
+   cin >> arr[i];
+}
+for(int i = 0;i < len;i++){
     maximum = max(maximum,arr[i]);//this is a predefine function to find maximum number
     minimum = min(minimum,arr[i]);//this is a predefine function to find minimum number
 }
@@ -15,3 +21,10 @@ cout << "maximum number is:" << maximum << endl;
 cout << "minimum number is:" << minimum << endl;
 return 0;
 }
+
+// output:
+// array[0]:1
+// array[1]:2
+// array[2]:3
+// maximum number is:3
+// minimum number is:1
