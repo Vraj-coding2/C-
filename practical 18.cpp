3ba1922fea26858fa68a9d27921d9826eaf556d8
Vrajcoding2 <vrajgajjar659@gmail.com>
2025-02-03 18:53:24 +0530

    practical 18

---
+++ practical 18.cpp
@@ -0,0 +1,27 @@
+// Program to illustrate default constructor, parameterized constructor and copy constructors.  
+#include<iostream>
+using namespace std;
+class student{
+  int rollno;
+  string name;
+  public:
+student(){ // default constructor
+  cout << "...This is a student Details function..." << endl;
+}
+ student(int r){ // this is a constructor for parametrized
+  rollno = r;
+ }
+
+ student(student &s1){
+  rollno = s1.rollno;
+  cout << "student rollno is :" << rollno;
+ }
+};
+int main(){ 
+ student s1; 
+int rollno = 10;
+student s2(rollno); // calling roll no constructor
+student s3(s2); // calling copy constructor passing the object
+
+return 0;
+}
